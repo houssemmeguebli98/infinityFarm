2a22d39703738367b168fd2f27ba6e292f0d1f3a
houssemmeguebli <megbli.houssam@gmail.com>
2023-11-07 14:43:12 +0100

    First Commit

---
+++ src/Entity/Materiel.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\JoinColumns;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\Table;
+
+
+#[ORM\Table(name: 'materiel', indexes: [new ORM\Index(name: 'idParc', columns: ['idParc'])])]
+#[Entity]
+class Materiel
+{
+    #[Id]
+    #[Column(name: "idMat", type: "integer", nullable: false)]
+    #[GeneratedValue(strategy: "IDENTITY")]
+    private int $idmat;
+
+    #[Column(name: "nomMat", type: "string", length: 255, nullable: false)]
+    private string $nommat;
+
+    #[Column(name: "etatMat", type: "string", length: 255, nullable: false)]
+    private string $etatmat;
+
+    #[Column(name: "QuantiteMat", type: "float", precision: 10, scale: 0, nullable: false)]
+    private float $quantitemat;
+
+    #[Column(name: "dateAjout", type: "date", nullable: false)]
+    private \DateTimeInterface $dateajout;
+
+    #[Column(name: "nomParc", type: "string", length: 255, nullable: false)]
+    private string $nomparc;
+    #[ManyToOne(targetEntity: Parc::class)]
+    #[JoinColumn(name: "idParc", referencedColumnName: "idParc")]
+    private Parc $idparc;
+
+    /**
+     * @return int
+     */
+    public function getIdmat(): int
+    {
+        return $this->idmat;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNommat(): string
+    {
+        return $this->nommat;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEtatmat(): string
+    {
+        return $this->etatmat;
+    }
+
+    /**
+     * @return float
+     */
+    public function getQuantitemat(): float
+    {
+        return $this->quantitemat;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getDateajout(): \DateTimeInterface
+    {
+        return $this->dateajout;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomparc(): string
+    {
+        return $this->nomparc;
+    }
+
+    /**
+     * @return Parc
+     */
+    public function getIdparc(): Parc
+    {
+        return $this->idparc;
+    }
+
+    /**
+     * @param int $idmat
+     */
+    public function setIdmat(int $idmat): void
+    {
+        $this->idmat = $idmat;
+    }
+
+    /**
+     * @param string $nommat
+     */
+    public function setNommat(string $nommat): void
+    {
+        $this->nommat = $nommat;
+    }
+
+    /**
+     * @param string $etatmat
+     */
+    public function setEtatmat(string $etatmat): void
+    {
+        $this->etatmat = $etatmat;
+    }
+
+    /**
+     * @param float $quantitemat
+     */
+    public function setQuantitemat(float $quantitemat): void
+    {
+        $this->quantitemat = $quantitemat;
+    }
+
+    /**
+     * @param \DateTimeInterface $dateajout
+     */
+    public function setDateajout(\DateTimeInterface $dateajout): void
+    {
+        $this->dateajout = $dateajout;
+    }
+
+    /**
+     * @param string $nomparc
+     */
+    public function setNomparc(string $nomparc): void
+    {
+        $this->nomparc = $nomparc;
+    }
+
+    /**
+     * @param Parc $idparc
+     */
+    public function setIdparc(Parc $idparc): void
+    {
+        $this->idparc = $idparc;
+    }
+
+}
---
+++ src/Entity/Parc.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Table(name: "parc")]
+#[Entity]
+class Parc
+{
+    #[Id]
+    #[Column(name: "idParc", type: "integer", nullable: false)]
+    #[GeneratedValue(strategy: "IDENTITY")]
+    private int $idparc;
+
+    #[Column(name: "nomParc", type: "string", length: 255, nullable: false)]
+    private string $nomparc;
+
+    #[Column(name: "adresseParc", type: "string", length: 255, nullable: false)]
+    private string $adresseparc;
+
+    #[Column(name: "superficieParc", type: "float", precision: 10, scale: 0, nullable: false)]
+    private float $superficieparc;
+
+
+
+
+    /**
+     * @return int
+     */
+    public function getIdparc(): int
+    {
+        return $this->idparc;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomparc(): string
+    {
+        return $this->nomparc;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdresseparc(): string
+    {
+        return $this->adresseparc;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSuperficieparc(): float
+    {
+        return $this->superficieparc;
+    }
+
+    /**
+     * @param int $idparc
+     */
+    public function setIdparc(int $idparc): void
+    {
+        $this->idparc = $idparc;
+    }
+
+    /**
+     * @param string $nomparc
+     */
+    public function setNomparc(string $nomparc): void
+    {
+        $this->nomparc = $nomparc;
+    }
+
+    /**
+     * @param string $adresseparc
+     */
+    public function setAdresseparc(string $adresseparc): void
+    {
+        $this->adresseparc = $adresseparc;
+    }
+
+    /**
+     * @param float $superficieparc
+     */
+    public function setSuperficieparc(float $superficieparc): void
+    {
+        $this->superficieparc = $superficieparc;
+    }
+
+
+    
+}
